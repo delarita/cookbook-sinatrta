e511e4b31f729758cb61bc8aa5bd62d7cd014f4f
julien daniel <j.delarita@gmail.com>
2020-10-26 16:42:33 +0100

    step

---
+++ app.rb
@@ -0,0 +1,19 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "pry-byebug"
+require "better_errors"
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path('..', __FILE__)
+end
+
+get '/' do
+  erb :index
+  #{}"<h1>Hello <em>world</em>!</h1>"
+end
+
+# [...]
+
+get '/about' do
+  erb :about
+end
